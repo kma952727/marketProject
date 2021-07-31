777d5442f82904bf15d1e7d00b67a0e1b51895be
kma952727 <kma952727@gmail.com>
2021-08-01 07:53:28 +0900

    add : index.html추가, thymeleaf fragment 추가

---
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -0,0 +1,17 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+public class MainController {
+
+	@GetMapping("/")
+	public String main() {
+		log.info("hello main");
+		return "index";
+	}
+}
