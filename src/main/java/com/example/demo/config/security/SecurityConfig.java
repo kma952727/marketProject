d923112fe963586386767736ba9721360ced1ae2
kma952727 <kma952727@gmail.com>
2021-09-02 09:37:25 +0900

    add : 요청시 로그인여부 확인후 로그인페이지이동 구현

--- src/main/java/com/example/demo/config/security/SecurityConfig.java
+++ src/main/java/com/example/demo/config/security/SecurityConfig.java
@@ -4,12 +4,14 @@ import javax.sql.DataSource;
 
 import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.reactive.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -30,18 +32,24 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	public BCryptPasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder(10);
 	}
-	
+	@Override
+    public void configure(WebSecurity web) throws Exception
+    {
+        web.ignoring().antMatchers("/css/**", "/image/**", "/js/**", "/product_image/**");
+    }
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 			.csrf()
 			.and()
 			.authorizeRequests()
-			.antMatchers("/").permitAll();
+			.antMatchers("/signin","/","/register").permitAll()
+			.anyRequest().authenticated();
 		http
 			.formLogin()
 			.loginPage("/signin")
 			.successHandler(successHandler());
+			//.permitAll();
 		http
 			.logout()
 			.logoutSuccessUrl("/")
